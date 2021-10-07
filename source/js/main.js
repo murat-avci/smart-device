b8e153957dd34ad12d16599eaa6201146dc5ce3b
murat-avci <avcmurat@yahoo.com>
2021-10-07 08:04:00 +0300

    fixed all bugs

--- source/js/main.js
+++ source/js/main.js
@@ -1,39 +1,42 @@
 const KEYCODE = {
-  esc: 27
+  esc: 27,
 };
-const link = document.querySelector('.page-header__contacts-button');
-const popup = document.querySelector('.modal');
-const popupClose = popup.querySelector('.modal__close');
-const form = popup.querySelector('.modal__form');
-const userName = popup.querySelector('#username');
-const phone = popup.querySelector('#call-phone');
+const link = document.querySelector(".page-header__contacts-button");
+const popup = document.querySelector(".modal");
+const popupClose = document.querySelector(".modal__close");
+const form = document.querySelector(".modal__form");
+const userName = document.querySelector("#username");
+const phone = document.querySelector("#call-phone");
 const askPhone = document.querySelector("#ask-us-phone");
 const phoneInputs = document.querySelectorAll('input[type^="tel"]');
-const message = document.querySelector('#call-question');
+const message = document.querySelector("#call-question");
 let isStorageSupport = true;
 let storage = {};
 
 const openPopup = function () {
-  popup.classList.add('modal--show');
-  document.body.classList.add('disable-scroll');
+  popup.classList.add("modal--show");
+  document.body.classList.add("disable-scroll");
 };
 
 const closePopup = function () {
-  popup.classList.remove('modal--show');
-  document.body.classList.remove('disable-scroll');
+  popup.classList.remove("modal--show");
+  document.body.classList.remove("disable-scroll");
 };
 
 try {
-  storage.name = localStorage.getItem('username');
-  storage.phone = localStorage.getItem('phone');
-  storage.message = localStorage.getItem('question');
+  storage.name = localStorage.getItem("username");
+  storage.phone = localStorage.getItem("phone");
+  storage.message = localStorage.getItem("question");
 } catch (err) {
   isStorageSupport = false;
 }
 
-link.addEventListener('click', function (evt) {
+link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  openPopup();
+
+  if (popup && form) {
+    openPopup();
+  }
 
   if (storage.name) {
     userName.value = storage.name;
@@ -45,29 +48,29 @@ link.addEventListener('click', function (evt) {
   }
 });
 
-popupClose.addEventListener('click', function (evt) {
+popupClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   closePopup();
 });
 
-form.addEventListener('submit', function () {
+form.addEventListener("submit", function () {
   if (isStorageSupport) {
-    localStorage.setItem('name', userName.value);
-    localStorage.setItem('phone', phone.value);
-    localStorage.setItem('message', message.value);
+    localStorage.setItem("name", userName.value);
+    localStorage.setItem("phone", phone.value);
+    localStorage.setItem("message", message.value);
   }
 });
 
-window.addEventListener('keydown', function (evt) {
+window.addEventListener("keydown", function (evt) {
   if (evt.key === KEYCODE.esc) {
     evt.preventDefault();
-    if (popup.classList.contains('modal--show')) {
+    if (popup.classList.contains("modal--show")) {
       closePopup();
     }
   }
 });
 
-popup.addEventListener('click', function (evt) {
+popup.addEventListener("click", function (evt) {
   if (evt.target === popup) {
     closePopup();
   }
@@ -79,63 +82,66 @@ const getCorrectName = function () {
 
   for (let i = 0; i < valueName.length; i++) {
     if (!re.test(valueName[i])) {
-      userName.setCustomValidity('Имя должно содержать только буквы');
+      userName.setCustomValidity("Имя должно содержать только буквы");
     } else {
-      userName.setCustomValidity('');
+      userName.setCustomValidity("");
     }
   }
 };
 
-userName.addEventListener('input', function () {
+userName.addEventListener("input", function () {
   getCorrectName();
 });
 
-
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener("DOMContentLoaded", function () {
   const inputs = document.querySelectorAll('input[type="tel"]');
   Array.prototype.forEach.call(inputs, function (input) {
     new InputMask({
       selector: input,
-      layout: input.dataset.mask
-    })
-  })
-})
+      layout: input.dataset.mask,
+    });
+  });
+});
 
 function InputMask(options) {
   this.el = this.getElement(options.selector);
-  if (!this.el) return
-  this.layout = options.layout || '+_ (___) ___-__-__';
+  if (!this.el) return;
+  this.layout = options.layout || "+_ (___) ___-__-__";
   this.maskreg = this.getRegexp();
 
   this.setListeners();
 }
 
 InputMask.prototype.getRegexp = function () {
-  let str = this.layout.replace(/_/g, '\\d');
-  str = str.replace(/\(/g, '\\(');
-  str = str.replace(/\)/g, '\\)');
-  str = str.replace(/\+/g, '\\+');
-  str = str.replace(/\s/g, '\\s');
+  let str = this.layout.replace(/_/g, "\\d");
+  str = str.replace(/\(/g, "\\(");
+  str = str.replace(/\)/g, "\\)");
+  str = str.replace(/\+/g, "\\+");
+  str = str.replace(/\s/g, "\\s");
 
   return str;
 };
 
 InputMask.prototype.mask = function (e) {
   let _this = e.target,
-      matrix = this.layout,
-      i = 0,
-      def = matrix.replace(/\D/g, ""),
-      val = _this.value.replace(/\D/g, "");
+    matrix = this.layout,
+    i = 0,
+    def = matrix.replace(/\D/g, ""),
+    val = _this.value.replace(/\D/g, "");
 
   if (def.length >= val.length) val = def;
 
-  _this.value = matrix.replace(/./g, function(a) {
-      return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a
+  _this.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length
+      ? val.charAt(i++)
+      : i >= val.length
+      ? ""
+      : a;
   });
 
-  if (e.type == 'blur') {
+  if (e.type == "blur") {
     const regexp = new RegExp(this.maskreg);
-    if (!regexp.test(_this.value)) _this.value = '';
+    if (!regexp.test(_this.value)) _this.value = "";
   } else {
     this.setCursorPosition(_this.value.length, _this);
   }
@@ -157,12 +163,12 @@ InputMask.prototype.setListeners = function () {
   this.el.addEventListener("input", this.mask.bind(this), false);
   this.el.addEventListener("focus", this.mask.bind(this), true);
   this.el.addEventListener("blur", this.mask.bind(this), false);
-}
+};
 
 InputMask.prototype.getElement = function (selector) {
   if (selector === undefined) return false;
   if (this.isElement(selector)) return selector;
-  if (typeof selector === 'string') {
+  if (typeof selector === "string") {
     const el = document.querySelector(selector);
     if (this.isElement(el)) return el;
   }
@@ -173,41 +179,47 @@ InputMask.prototype.isElement = function (element) {
   return element instanceof Element || element instanceof HTMLDocument;
 };
 
-function mouse (inputs) {
-  inputs.forEach (input => {
+function mouse(inputs) {
+  inputs.forEach((input) => {
     input.addEventListener("focus", function () {
       input.placeholder = "+7_ (___) ___-__-__";
-    })
+    });
 
     input.addEventListener("mouseover", function () {
       input.placeholder = "+7_ (___) ___-__-__";
-    })
+    });
 
     input.addEventListener("mouseout", function () {
       input.placeholder = "Телефон";
-    })
-  })
+    });
+  });
 }
 
 mouse(phoneInputs);
 
-const accordionItems = document.querySelectorAll('.accordion');
-const accordionPanes = document.querySelectorAll('.accordion__pane');
+const accordionItems = document.querySelectorAll(".accordion");
+const accordionPanes = document.querySelectorAll(".accordion__pane");
 
 const hidePane = function (button, pane) {
-  button.classList.add('accordion__toggle--inactive');
-  pane.classList.add('accordion__pane--hidden');
+  button.classList.add("accordion__toggle--inactive");
+  pane.classList.add("accordion__pane--hidden");
 };
 
 const showPane = function (button, pane) {
-  button.classList.remove('accordion__toggle--inactive');
-  pane.classList.remove('accordion__pane--hidden');
+  button.classList.remove("accordion__toggle--inactive");
+  pane.classList.remove("accordion__pane--hidden");
 };
 
 const toggleAccordion = function (evt) {
   Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
-    const button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
-    if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+    const button = accordionPane
+      .closest(".accordion")
+      .querySelector(".accordion__toggle");
+    if (
+      (button === evt.target &&
+        !button.classList.contains("accordion__toggle--inactive")) ||
+      button !== evt.target
+    ) {
       hidePane(button, accordionPane);
     } else if (button === evt.target) {
       showPane(button, accordionPane);
@@ -216,8 +228,8 @@ const toggleAccordion = function (evt) {
 };
 
 Array.prototype.forEach.call(accordionItems, function (accordion) {
-  const accordionToggleButton = accordion.querySelector('.accordion__toggle');
-  const accordionPane = accordion.querySelector('.accordion__pane');
+  const accordionToggleButton = accordion.querySelector(".accordion__toggle");
+  const accordionPane = accordion.querySelector(".accordion__pane");
   hidePane(accordionToggleButton, accordionPane);
-  accordionToggleButton.addEventListener('click', toggleAccordion);
+  accordionToggleButton.addEventListener("click", toggleAccordion);
 });
